464b8cbc53699d86b2447c3cd1ea63b63e5b03d6
mzhKU <ma.hed@bluewin.ch>
2023-02-27 17:17:12 +0100

    Initial

---
+++ src/main/java/ch/mzh/Main.java
@@ -0,0 +1,11 @@
+package ch.mzh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
