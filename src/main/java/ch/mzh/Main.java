7fd82c65f46a3c592e35fa44c3ae33b095d6cb65
Martin Hediger <hediger@swissquant.com>
2023-03-16 09:56:51 +0100

    intial

--- src/main/java/ch/mzh/Main.java
+++ src/main/java/ch/mzh/Main.java
@@ -1,11 +1,16 @@
 package ch.mzh;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
